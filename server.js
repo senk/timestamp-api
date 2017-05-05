b9bea9cf3bb11935684f21b87db45317dfb0d65d
Robin Naundorf <r.naundorf@fh-muenster.de>
2017-05-05 17:20:02 +0000

    finished

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require("express")
 
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var port = process.env.PORT || 8080;
+
+app.get('/:data', function (req, res) {
+  var date = req.params.data;
+  if (/^\d+$/.test(date)) {
+    date = parseInt(date*1000)
+  }
+  date = new Date(date)
+  if (date != "Invalid Date") {
+    var message = { "unix": date.getTime(), "natural": date.toDateString() };
+  } else {
+    var message = { "unix": null, "natural": null }
+  }
+  res.send(JSON.stringify(message));
 })
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 8080!')
 })
